c1a17b71ed911db54c34bc6d92e73fc1e1c13388
mariathathiana <109003131+mariathathiana@users.noreply.github.com>
2024-09-17 20:27:15 +0200

    Array for calculate a sum of numbers

---
+++ Aritmetica.java
@@ -0,0 +1,13 @@
+public class Aritmetica {
+    public static void main(String[] args) {
+        
+int [] sumarNumeros = {1,2,3,4,5};
+int sum = 0;
+for(int i=0; i< sumarNumeros.length; i++){
+    sum += sumarNumeros[i];
+    }
+
+    System.out.println(sum);
+
+}
+}
